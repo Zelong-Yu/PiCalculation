a031b2a5ca52643a3012f13bccf610ff519074fa
Zelong Yu <zelong.yu@outlook.com>
2019-03-28 21:55:38 -0400

    made a menum, and made naive approach (array creation) an option

--- PiCalculation/Program.cs
+++ PiCalculation/Program.cs
@@ -10,6 +10,64 @@ namespace PiCalculation
     {
         static void Main(string[] args)
         {
+            MainAppLoop();
+        }
+
+            //Create a function which takes an x, y coordinate struct and returns a double 
+            // corresponding to the hypotenuse of a triangle with sides of lengths x, y.
+            //Func<XYCoord, double> hypotenuse = n => Math.Sqrt(Math.Pow(n.x, 2) + Math.Pow(n.y, 2));
+        public static double hypotenuse(XYCoord n) => Math.Sqrt(Math.Pow(n.x, 2) + Math.Pow(n.y, 2));
+
+        private static void MainAppLoop()
+        {
+            bool End = false;
+            List<string> calculationMethods = new List<string>();
+            calculationMethods.Add("Naïve approach (O(N) time, O(N) space: Array N created and iterated)");
+            calculationMethods.Add("Space improved (O(N) time, O(1) space)");
+            do
+            {
+                Console.Clear();
+                int selected = UI.SelectionMenu(calculationMethods);
+                End = HandleSelection(selected);
+
+            } while (!End);
+        }
+
+        private static void NaiveApproach()
+        {
+            Console.Clear();
+            //takes one int parameter from the command line and creates an array of that 
+            //length containing randomly initialized coordinates.
+            int numPoints = UI.AcceptValidInt("How many points to scatter?\n>", 1);
+
+            Random rnd = new Random();
+            XYCoord[] points = new XYCoord[numPoints];
+            for (int i = 0; i < numPoints; ++i)
+            {
+                points[i] = new XYCoord(rnd);
+            }
+            //Iterate over the array, incrementing a counter for each coordinate which overlaps the unit circle.
+            double estimatePi = points.Count(p => hypotenuse(p) < 1.0) / (double)numPoints * 4;
+            Console.WriteLine($"Estimated Pi Value: {estimatePi}\n" +
+                              $"Actual Pi Value:    {Math.PI}\n" +
+                              $"Difference:         {Math.Abs(estimatePi - Math.PI)}");
+        }
+
+        private static bool HandleSelection(int selected)
+        {
+            switch (selected)
+            {
+                case -1:
+                    return true;
+                case 0:
+                    NaiveApproach();
+                    return false;
+                    break;
+                default:
+                    return false;
+                    break;
+            }
+
         }
     }
 }
---
+++ PiCalculation/XYCoord.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiCalculation
+{
+    //1.	Create a struct to hold x, y coordinates as doubles. Provide a 2-parameter constructor
+    // which takes a new set of coordinates and a 1-parameter constructor which takes a Random object
+    //    and uses the NextDouble method to initialize X and Y
+    struct XYCoord
+    {
+        public double x;
+        public double y;
+
+        public XYCoord(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public XYCoord(Random r)
+        {
+            this.x = r.NextDouble();
+            this.y = r.NextDouble();
+        }
+
+        public XYCoord((double x, double y) t) : this(t.x, t.y) { }
+
+        public void Deconstruct(out double x, out double y)
+        {
+            x = this.x;
+            y = this.y;
+        }
+        
+    }
+
+}
